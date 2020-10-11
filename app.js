165542007b7eb4fe0a25c5c3b62f4c7d90de6aad
Binyamin Houri <bini@Binis-Macbook.local>
2020-10-11 14:29:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var pl = require("tau-prolog");
+require("tau-prolog/modules/lists")(pl);
+
+const hbs = require('hbs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Define public ,views and partials path
+const publicPath = path.join(__dirname, './public');
+const viewsPath = path.join(__dirname, './views')
+
+app.set('views', viewsPath)
+app.use(express.static(publicPath));
+
+//Setup the hbs engine
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+  res.render('tictactoe');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
